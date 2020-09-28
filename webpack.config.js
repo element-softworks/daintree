de8eacb03bff01b8d3faba38815ef9b790fba1e8
Joe <contact@joemethven.com>
2020-09-28 13:18:30 +0100

    Fixed exporting of components

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
 	output: {
 		filename: 'index.js',
 		path: path.resolve(__dirname, 'dist'),
-		library: 'daintree',
+		library: '',
 		libraryTarget: 'commonjs'
 	},
 	optimization: {
