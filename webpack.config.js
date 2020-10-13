99cc6a00dbb9e0382e42f168953a528653d5712a
Joe <contact@joemethven.com>
2020-10-13 21:01:21 +0100

    Fixed SSR for production environments

--- webpack.config.js
+++ webpack.config.js
@@ -2,120 +2,129 @@ const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const nodeExternals = require('webpack-node-externals');
 const autoprefixer = require('autoprefixer');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-	entry: './src/components/index.ts',
-	externals: [nodeExternals()],
-	output: {
-		filename: 'index.js',
-		path: path.resolve(__dirname, 'dist'),
-		library: '',
-		libraryTarget: 'commonjs',
-	},
-	optimization: {
-		minimize: false,
-	},
-	plugins: [new CleanWebpackPlugin()],
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				use: ['babel-loader'],
-			},
-			{
-				test: /\.(ts|tsx)$/,
-				use: 'ts-loader',
-				exclude: /node_modules/,
-			},
-			{
-				test: /\.scss$/,
-				exclude: /\.module\.scss$/,
-				use: [
-					'style-loader',
-					{
-						loader: 'css-loader',
-						options: {
-							modules: false,
-							sourceMap: true,
-						},
-					},
-					{
-						loader: 'postcss-loader',
-						options: {
-							postcssOptions: {
-								ident: 'postcss',
-								sourceMap: true,
-								plugins: () => [autoprefixer({})],
-							},
-						},
-					},
-					{
-						loader: 'sass-loader',
-						options: {
-							sassOptions: {
-								includePaths: [
-									`${__dirname}/node_modules`,
-									`${__dirname}/src/`,
-									`${__dirname}/src/styles`,
-								],
-							},
-						},
-					},
-				],
-			},
-			{
-				test: /\.module\.scss$/,
-				use: [
-					'style-loader',
-					{
-						loader: 'css-loader',
-						options: {
-							modules: {
-								exportLocalsConvention: 'camelCase',
-								localIdentName: '[local]__[hash:base64:5]',
-							},
-							sourceMap: true,
-						},
-					},
-					{
-						loader: 'postcss-loader',
-						options: {
-							postcssOptions: {
-								ident: 'postcss',
-								sourceMap: true,
-								plugins: () => [autoprefixer({})],
-							},
-						},
-					},
-					{
-						loader: 'sass-loader',
-						options: {
-							sassOptions: {
-								includePaths: [
-									`${__dirname}/node_modules`,
-									`${__dirname}/src/`,
-									`${__dirname}/src/styles`,
-								],
-							},
-						},
-					},
-				],
-			},
-		],
-	},
-	resolve: {
-		alias: {
-			'@components': path.resolve(__dirname, './src/components'),
-			'@reducers': path.resolve(__dirname, './src/reducers'),
-			'@helpers': path.resolve(__dirname, './src/helpers'),
-			'@actions': path.resolve(__dirname, './src/actions'),
-			'@images': path.resolve(__dirname, './src/images'),
-			'@styles': path.resolve(__dirname, './src/styles'),
-			'@fonts': path.resolve(__dirname, './src/fonts'),
-			'@views': path.resolve(__dirname, './src/views'),
-			'@store': path.resolve(__dirname, './src/store.js'),
+module.exports = (env) => {
+	const plugins = [new CleanWebpackPlugin()];
+
+	if (env.production) {
+		plugins.push(new MiniCssExtractPlugin());
+	}
+
+	return {
+		entry: './src/components/index.ts',
+		externals: [nodeExternals()],
+		output: {
+			filename: 'index.js',
+			path: path.resolve(__dirname, 'dist'),
+			library: '',
+			libraryTarget: 'commonjs',
 		},
-		extensions: ['.tsx', '.ts', '.js'],
-	},
+		optimization: {
+			minimize: false,
+		},
+		plugins,
+		module: {
+			rules: [
+				{
+					test: /\.(js|jsx)$/,
+					exclude: /node_modules/,
+					use: ['babel-loader'],
+				},
+				{
+					test: /\.(ts|tsx)$/,
+					use: 'ts-loader',
+					exclude: /node_modules/,
+				},
+				{
+					test: /\.scss$/,
+					exclude: /\.module\.scss$/,
+					use: [
+						env.production ? MiniCssExtractPlugin.loader : 'style-loader',
+						{
+							loader: 'css-loader',
+							options: {
+								modules: false,
+								sourceMap: true,
+							},
+						},
+						{
+							loader: 'postcss-loader',
+							options: {
+								postcssOptions: {
+									ident: 'postcss',
+									sourceMap: true,
+									plugins: () => [autoprefixer({})],
+								},
+							},
+						},
+						{
+							loader: 'sass-loader',
+							options: {
+								sassOptions: {
+									includePaths: [
+										`${__dirname}/node_modules`,
+										`${__dirname}/src/`,
+										`${__dirname}/src/styles`,
+									],
+								},
+							},
+						},
+					],
+				},
+				{
+					test: /\.module\.scss$/,
+					use: [
+						env.production ? MiniCssExtractPlugin.loader : 'style-loader',
+						{
+							loader: 'css-loader',
+							options: {
+								modules: {
+									exportLocalsConvention: 'camelCase',
+									localIdentName: '[local]__[hash:base64:5]',
+								},
+								sourceMap: true,
+							},
+						},
+						{
+							loader: 'postcss-loader',
+							options: {
+								postcssOptions: {
+									ident: 'postcss',
+									sourceMap: true,
+									plugins: () => [autoprefixer({})],
+								},
+							},
+						},
+						{
+							loader: 'sass-loader',
+							options: {
+								sassOptions: {
+									includePaths: [
+										`${__dirname}/node_modules`,
+										`${__dirname}/src/`,
+										`${__dirname}/src/styles`,
+									],
+								},
+							},
+						},
+					],
+				},
+			],
+		},
+		resolve: {
+			alias: {
+				'@components': path.resolve(__dirname, './src/components'),
+				'@reducers': path.resolve(__dirname, './src/reducers'),
+				'@helpers': path.resolve(__dirname, './src/helpers'),
+				'@actions': path.resolve(__dirname, './src/actions'),
+				'@images': path.resolve(__dirname, './src/images'),
+				'@styles': path.resolve(__dirname, './src/styles'),
+				'@fonts': path.resolve(__dirname, './src/fonts'),
+				'@views': path.resolve(__dirname, './src/views'),
+				'@store': path.resolve(__dirname, './src/store.js'),
+			},
+			extensions: ['.tsx', '.ts', '.js'],
+		},
+	};
 };
